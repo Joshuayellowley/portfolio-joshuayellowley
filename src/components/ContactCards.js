646675f8d0ed7340a71ec405cdf5eacbe1a967a1
Joshua Yellowley <Joshuayellowley@gmail.com>
2020-09-11 19:19:00 -0700

    Stable Website Deploy Sept 11 2020 Updated

---
+++ src/components/ContactCards.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './ContactCards.css';
+import CardItem from './CardItem';
+
+function ContactCards() {
+    return (
+        <div className='cards'>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/linkedin.jpg'
+                            text='LinkedIn'
+                            label='Social'
+                            path='https://www.linkedin.com/in/joshua-yellowley-3739b5141/'
+                        />
+                        {/* <CardItem
+                            src='images/clipboard.jpg'
+                            text='Resume'
+                            label='Resume'
+                            path='/'
+                        /> */}
+                        <CardItem
+                            src='images/facebook.png'
+                            text='Facebook'
+                            label='Social'
+                            path='https://www.facebook.com/joshua.yellowley/'
+                        />
+                        <CardItem
+                            src='images/twitter.png'
+                            text='Twitter'
+                            label='Social'
+                            path='https://twitter.com/JoshuaYellowley'
+                        />
+                        <CardItem
+                            src='images/instagram.jpg'
+                            text='Instagram'
+                            label='Social'
+                            path='https://www.instagram.com/joshua_yellowley/?hl=en'
+                        />
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default ContactCards;
